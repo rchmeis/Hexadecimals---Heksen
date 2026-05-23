a5226ddb5d78b120bdfc950c7310f7ea5477628e
rchmeis <r.chmeis@gmail.com>
2026-05-23 14:18:21 +0200

    Code cleanup

---
+++ Hexadecimals - Heksen/HexValidator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hexadecimals___Heksen
+{
+    internal class HexValidator
+    {
+        public void validate(string hex)
+        {
+            char[] validCharacters = new char[]
+            {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','A', 'B', 'C', 'D', 'E', 'F'
+            };
+
+
+            bool hasHashtag = hex[0] == '#';
+            bool is7chars = hex.Length == 7;
+            bool validChars = true;
+
+            for (int i = 1; i < hex.Length; i++)
+            {
+                bool valid = false;
+                foreach(char c in validCharacters)
+                {
+                    if (c == hex[i])
+                    {
+                        valid = true;
+                        break;
+                    }        
+                }
+                if (!valid)
+                {
+                    validChars = false;
+                }
+            }
+
+            if (hasHashtag && validChars && is7chars)
+            {
+                Console.WriteLine("The HEX value is valid");
+            }
+            if (!hasHashtag || !validChars || !is7chars)
+            {
+                Console.WriteLine("The HEX value is invalid");
+                if (!hasHashtag)
+                {
+                    Console.WriteLine("A HEX value must start with #");
+                }
+                if (!is7chars)
+                {
+                    Console.WriteLine("A HEX value must be exactly 7 characters long");
+                }
+                if (!validChars)
+                {
+                    Console.WriteLine("HEX values only contains numbers and letters within the ranges 0-9 and A-Z");
+                }
+            }
+        }
+    }
+}
--- Hexadecimals - Heksen/Program.cs
+++ Hexadecimals - Heksen/Program.cs
@@ -1,24 +1,17 @@
-﻿namespace Hexadecimals___Heksen
+﻿using System.Security.Cryptography;
+
+namespace Hexadecimals___Heksen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //This program will verify if a string is a valid hexadecimal and color.
-            //For each requirement a method is created to verify that the requirement is met.
+            Console.Write("Indtast den HEX værdi du ønsker at validere: ");
+            string hex = Console.ReadLine().Trim().ToUpper();
+
+            HexValidator validator = new HexValidator();
+            validator.validate(hex);
             
-            Console.WriteLine("Input value to verify if it is a valid hexidecimal and color: ");
-            string input = Console.ReadLine().ToLower();
-
-            bool indexZero = Check.Index0(input);            
-            bool length = Check.Length(input);
-            bool HexValue = Check.ValidHex(input);
-            
-            if(indexZero && length && HexValue)
-            { Console.WriteLine("This is a valid hexcolor."); }
-                
-
-
         }
     }
 }
